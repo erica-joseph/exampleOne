4b13c49d039f09b1b8c1c5c67e55ed6f7e5d3e5a
erica-joseph <ericagjoseph@outlook.com>
2021-06-05 19:18:42 -0400

    Correction

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,3 @@
-package org.example;
 import java.util.Scanner;
 /**
  *  UCF COP3330 Summer 2021 Assignment 1 Solution
@@ -9,9 +8,9 @@ public class App
     public static void main( String[] args )
     {
         Scanner sc= new Scanner(System.in);
-        System.out.print( "What is your name?" );
+        System.out.print( "What is your name? " );
         String str= sc.nextLine();
-        System.out.print( "Hello,"+str);
-        System.out.print(", nice to meet you!");
+        System.out.print( "Hello, "+str);
+        System.out.print(" , nice to meet you!");
     }
 }
